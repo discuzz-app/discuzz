b68deeeb251938d0fa71b0da6fc907d2fbceba4e
MartinPham <MartinPham@users.noreply.github.com>
2022-01-06 19:53:48 +0000

    Automated Pages build a484a69

--- packages/core/src/components/AuthProvider/hooks/useAuth.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react'
-import { AuthContext } from '../contexts/AuthContext'
-
-export const useAuth = () => {
-  const auth = useContext(AuthContext)
-
-  return auth
-}
--- packages/core/src/types/PostAuthor.ts
+++
@@ -1,5 +0,0 @@
-export type PostAuthor = {
-  id: string;
-  name: string | null;
-  photoUrl?: string;
-};

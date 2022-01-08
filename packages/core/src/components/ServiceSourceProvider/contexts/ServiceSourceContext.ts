c33906c00f5e0fc01c8beed7c53ee5ce8316eef8
MartinPham <MartinPham@users.noreply.github.com>
2022-01-08 00:43:03 +0000

    Automated Pages build 207a661

--- packages/core/src/components/ServiceSourceProvider/contexts/ServiceSourceContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react'
-import { Auth, Data } from 'services/source'
-
-export type ServiceSource = {
-  auth?: Auth;
-  data?: Data;
-};
-export const ServiceSourceContext = createContext<ServiceSource>({})
